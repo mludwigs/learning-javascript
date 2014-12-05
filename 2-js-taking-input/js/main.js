7eb7d5d0f3be8ad7fcd526d0ca3a42e5d033e6c6
Caleb Varoga <cvaroga@cvaroga.local>
2014-12-05 13:48:53 -0700

    Added Event Listener for adding a paragraph, capturing user input, but only creating paragraphs with the first user element, need to figure out how to make uMessage dynamic based on order of inputs in prompt.

--- 2-js-taking-input/js/main.js
+++ 2-js-taking-input/js/main.js
@@ -6,10 +6,29 @@ window.onload = function() {
 		document.getElementById("changeMe").innerHTML = ("hi there, " + uMessage + ".");
 		document.getElementById("clickMe").id = "clicked";
 		document.getElementById("clicked").innerHTML = ("add some more.");
+		function letsCreate() {
+			var main = document.getElementById("text");
+			var newP = document.createElement("p");
+			newP.appendChild(document.createTextNode(uMessage));
+			main.appendChild(newP);
+		}
+
+		var clicked = document.getElementById("clicked");
+		clicked.addEventListener("click", letsCreate);
 	}
 
 	// Registering Click Event on Button and Calling the Above Function.
 	var clickMe = document.getElementById("clickMe");
 	clickMe.addEventListener("click", letsChange);
+
+	// function letsCreate() {
+	// 	var main = document.getElementById("main");
+	// 	var newP = document.createElement("p");
+	// 	newP.appendChild(document.createTextNode(uMessage));
+	// 	main.appendChild(newP);
+	// }
+	//
+	// var clicked = document.getElementById("clicked");
+	// clicked.addEventListener("click", letsCreate);
 
 }
