0fac11f86e79d60e49558ca756e1881fd15c7aa9
Caleb Varoga <cvaroga@cvaroga.local>
2014-12-08 13:23:20 -0700

    Added event listener on submit, and added storyForm.reset(); to clear the input field each time a line is added.

--- 2-js-taking-input/js/main.js
+++ 2-js-taking-input/js/main.js
@@ -1,39 +1,20 @@
 window.onload = function() {
 
-	var form = document.getElementById("form");
+	// Variable Declarations
+	var storyForm = document.getElementById("storyForm");
 	var uInput = document.getElementById("uMessage");
-	form.onsubmit = function(e) {
+
+	// Function Declaration
+	var addP = function(e) {
 		e.preventDefault();
-		console.log(uInput.value);
 		var main = document.getElementById("text");
 		var newP = document.createElement("p");
 		newP.innerHTML = uInput.value
 		main.appendChild(newP);
+		storyForm.reset();
 	};
 
+	// Calling the function on Submit
+	storyForm.addEventListener("submit", addP);
+
 }
-
-	// Old Function - Trying New Solution
-	// // Function to change the H1, button ID, and add a paragraph.
-	// function letsChange() {
-	// 	var uMessage = prompt("What's Your Name?");
-		// document.getElementById("changeMe").innerHTML = ("hi there, " + uMessage + ".");
-	// 	document.getElementById("clickMe").id = "clicked";
-	// 	document.getElementById("clicked").innerHTML = ("add some more.");
-	// 	function letsCreate() {
-	// 		uMessage = prompt("What would you like to add?")
-	// 		var main = document.getElementById("text");
-	// 		var newP = document.createElement("p");
-	// 		newP.appendChild(document.createTextNode(uMessage));
-	// 		main.appendChild(newP);
-	// 	}
-	// 	var clicked = document.getElementById("clicked");
-	// 	clicked.addEventListener("click", letsCreate);
-	// }
-
-	// // Registering Click Event Listener on Button and Calling the Above Function.
-	// var clickMe = document.getElementById("clickMe");
-	// clickMe.addEventListener("click", letsChange);
-	// // Registering Click Event Listner on Button AFTER ID Change
-	// var clicked = document.getElementById("clicked");
-	// clicked.addEventListener("click", letsCreate);
