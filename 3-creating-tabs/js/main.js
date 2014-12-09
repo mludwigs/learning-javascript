f36fa7c11993e0655785c7cf11feaa1aa4776cd0
Caleb Varoga <cvaroga@cvaroga.melteast.meltmedia.com>
2014-12-09 12:02:05 -0700

    Adding all baseline-code to start tabs project, as well as new JS setup.

--- 3-creating-tabs/js/main.js
+++ 3-creating-tabs/js/main.js
@@ -1,3 +1,27 @@
-$(document).ready(function(){
+(function() {
 
-});
+  var
+    testEl,
+    clickButton;
+
+  function setPageVariables() {
+    testEl = document.getElementById("test");
+    clickButton = document.getElementById("clickBtn");
+  }
+
+  function showDialog() {
+    testEl.style.display = "block";
+  }
+
+  function setPageEvents() {
+    
+  }
+
+  function runPageFunctions() {
+    setPageVariables();
+    setEvents();
+  }
+
+  window.addEventListener("load", runPageFunctions);
+
+})();
