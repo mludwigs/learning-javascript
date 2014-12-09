fb4db35bd90bcb7140b86fe9dd85472090c080d1
Caleb Varoga <cvaroga@cvaroga.melteast.meltmedia.com>
2014-12-08 17:01:39 -0700

    Working on removing all global variables.

--- 2-js-taking-input/js/main.js
+++ 2-js-taking-input/js/main.js
@@ -1,33 +1,27 @@
-var storyLines = [];
-
-var createNewParagraph = function(paragraphHTML) {
-	var newP = document.createElement("p");
-	newP.innerHTML = paragraphHTML;
-	storyText.appendChild(newP);
-}
-
 // Forcing wait upon window load.
 window.onload = function() {
-
-	// Variable Declarations
-	var storyForm = document.getElementById("storyForm");
-	var uInput = document.getElementById("uInput");
+	// List of lines in the story (used for reverse function).
+	var storyLines = [];
+	// Function to create new paragraphs based on user input.
+	var createNewParagraph = function(paragraphHTML) {
+		var newP = document.createElement("p");
+		newP.innerHTML = paragraphHTML;
+		storyText.appendChild(newP);
+	}
 	var storyText = document.getElementById("storyText");
-
+	var storyForm = document.getElementById("storyForm");
 	// Function Declaration
 	var addP = function(e) {
 		e.preventDefault();
+		var uInput = document.getElementById("uInput");
 		createNewParagraph(uInput.value);
 		storyLines.push(uInput.value);
 		storyForm.reset();
 	};
-
 	// Calling the function on Submit
 	storyForm.addEventListener("submit", addP);
-
 	// Creating variable for reverse button.
 	var reverse = document.getElementById("clickMe");
-
 	// Function to reverse paragraph elements stored within storyLines array.
 	var reverseReverse = function() {
 		while (storyText.firstChild) {
@@ -38,7 +32,6 @@ window.onload = function() {
 			createNewParagraph(storyLines[i]);
 		}
 	}
-
 	// Calling the function on Reverse, Reverse
 	reverse.addEventListener("click", reverseReverse)
 }
