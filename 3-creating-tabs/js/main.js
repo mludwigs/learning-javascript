d732ac4040b46c50a0ec0270c2be8bd1b19ff469
Caleb Varoga <cvaroga@cvaroga.local>
2014-12-22 16:54:21 -0700

    Set up tab class/object pair, and starting TabManager object/function.

--- 3-creating-tabs/js/main.js
+++ 3-creating-tabs/js/main.js
@@ -1,5 +1,43 @@
 (function() {
 
-  
+  function TabManager(optionsObj) {
+
+    this.options = optionsObj;
+
+  }
+
+  TabManager.prototype = {
+
+    
+
+  }
+
+  function Tab(isActive, elem, id) {
+
+    this.isActive = isActive;
+
+    this.elem = elem;
+
+    this.id = id;
+
+  }
+
+  // Tab is a Class
+
+  Tab.prototype = {
+
+    isClicked: function() {
+
+    }
+
+  }
+
+  var tab = new Tab(true, "1", "tab1");
+
+  console.log(tab.isActive);
+  console.log(tab.elem);
+  var tab2 = new Tab(false, "2", "tab2");
+  console.log(tab2.isActive);
+  console.log(tab2.elem);
 
 })();
